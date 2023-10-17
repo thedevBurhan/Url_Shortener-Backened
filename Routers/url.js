b7be0587153d33c9dcd8cb8e48dc3330b6892d7b
thedevBurhan <smdburhan25@gmail.com>
2023-10-17 12:00:15 +0530

    changes is delete id url

--- Routers/url.js
+++ Routers/url.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {generateNewShortURL,handleGetAnalytics,handleDeleteUrl} from "../Url_Shortener/url.js"
+import {generateNewShortURL,handleGetAnalytics} from "../Url_Shortener/url.js"
 
 //initalize the router
 const router=express.Router();
@@ -7,5 +7,18 @@ const router=express.Router();
 router.post("/",generateNewShortURL);
 
 router.get("/analytics/:shortId",handleGetAnalytics);
-router.delete("/delete/:_id",handleDeleteUrl);
+// // to delete a URL data
+router.delete("/deleteUrl/:id",async(req,res)=>{
+    try {
+         const {id}=req.params;
+         if(!id){
+           return res.status(400).json({data:"Wrong Request"})  
+         }
+         const result=await deleteUrlData(id);
+         res.status(200).json({data:{result:result,message:"Deteled URl Sucessfully"}})
+       } catch (error) {
+         console.log(`${error} No URL is Deleted`)
+         res.status(500).json({data:"Internal Server Error"})
+       }
+  })
 export const urlRouter=router;
--- Routers/users.js
+++ Routers/users.js
@@ -85,17 +85,17 @@ router.delete("/delete/:id",async(req,res)=>{
   })
 
   // // to delete a URL data
-  // router.delete("/deleteUrl/:id",async(req,res)=>{
-  //   try {
-  //        const {id}=req.params;
-  //        if(!id){
-  //          return res.status(400).json({data:"Wrong Request"})  
-  //        }
-  //        const result=await deleteUrlData(id);
-  //        res.status(200).json({data:{result:result,message:"Deteled URl Sucessfully"}})
-  //      } catch (error) {
-  //        console.log(`${error} No URL is Deleted`)
-  //        res.status(500).json({data:"Internal Server Error"})
-  //      }
-  // })
+  router.delete("/deleteUrl/:id",async(req,res)=>{
+    try {
+         const {id}=req.params;
+         if(!id){
+           return res.status(400).json({data:"Wrong Request"})  
+         }
+         const result=await deleteUrlData(id);
+         res.status(200).json({data:{result:result,message:"Deteled URl Sucessfully"}})
+       } catch (error) {
+         console.log(`${error} No URL is Deleted`)
+         res.status(500).json({data:"Internal Server Error"})
+       }
+  })
 export const usersRouter=router;  
